f61f6ab0f02f64c2c9a081193926c3a77ac65a2e
jmorrow4 <70256544+jmorrow4@users.noreply.github.com>
2020-09-27 22:46:56 -0700

    Add files via upload
    test 4

--- AwwGeezMan.cpp
+++ AwwGeezMan.cpp
@@ -53,13 +53,11 @@ int main (int ac, char** av) {
 		demension = *(av + ac - 1);
 
 		if (demension.compare(s1) == 0) {
-			using namespace C137;
 			cout << "Morty C137 says:" << endl;
 	
 			C137::Morty(start1, stop1, 1);
 		}
 		else if (demension.compare(s2) == 0) {
-			using namespace Z286;
 			cout << "Morty Z286 says:" << endl;
 			
 			Z286::Morty(start1, stop1, 1);
@@ -77,13 +75,11 @@ int main (int ac, char** av) {
 		demension = *(av + ac - 1);
 	
 		if (demension.compare(s1) == 0) {
-			using namespace C137;
 			cout << "Morty C137 says:" << endl;
 			
 			C137::Morty(start1, stop1, step1);
 		}
 		else if (demension.compare(s2) == 0) {
-			using namespace Z286;
 			cout << "Morty Z286 says:" << endl;
 			
 			Z286::Morty(start1, stop1, step1);
